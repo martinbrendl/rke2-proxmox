e7e45e9cdc8c3db8914cd51262aaeed48d6686cf
martinbrendl <mabrendl@gmail.com>
2026-04-10 22:21:09 +0200

    Update Rancher hostname and Let's Encrypt email

--- rke2-cilium.sh
+++ rke2-cilium.sh
@@ -57,10 +57,10 @@ lbrangeStop=10.0.0.230
 certName=id_ed25519
 
 # Rancher hostname (with Let's Encrypt SSL)
-rancherHostname=rancher.xbm.cz
+rancherHostname=rancher.example.com
 
 # Email for Let's Encrypt (required for certificate)
-letsencryptEmail=info@xbm.cz
+letsencryptEmail=info@example.com
 
 #############################################
 #            DO NOT EDIT BELOW              #
